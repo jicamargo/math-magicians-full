d4448a81ed75514eeea1b9f31188ba863681e465
jic <ji_camargo@yahoo.com>
2023-06-14 14:39:36 -0500

    fix ES and CSS linter errora

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,9 @@ module.exports = {
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
+  ignorePatterns: [
+    "dist/"
+  ],
   rules: {
     'react-refresh/only-export-components': 'warn',
   },
